f954e69755492310684d9a282dc55a1e7f6ce353
Kazenin <kazeninstorm7@gmail.com>
2026-04-29 16:55:37 -0300

    refactor: melhora legibilidade e extrai regra de quantidade

--- ItemPedido.java
+++ ItemPedido.java
@@ -1,17 +1,21 @@
 public class ItemPedido {
 
-    private Produto produto;
-    private int quantidade;
+    private final Produto produto;
+    private final int quantidade;
 
     public ItemPedido(Produto produto, int quantidade) {
 
-        if (quantidade <= 0) {
-            System.out.println("Quantidade inválida, ajustando para 1");
-            quantidade = 1;
+        this.produto = produto;
+        this.quantidade = corrigirQuantidadeSeNecessario(quantidade);
+    }
+
+    private int corrigirQuantidadeSeNecessario(int quantidadeInformada) {
+
+        if (quantidadeInformada <= 0) {
+            return 1;
         }
 
-        this.produto = produto;
-        this.quantidade = quantidade;
+        return quantidadeInformada;
     }
 
     public double getSubtotal() {
