845f27e2edc609ab942fe3d3e6edc04b845e137b
kazeninstorm7-ux <kazeninstorm7@gmail.com>
2026-04-29 21:38:22 -0300

    Delete PedidoTeste.java

--- PedidoTeste.java
+++
@@ -1,9 +0,0 @@
-@Test
-void deveCalcularTotalCorretamente() {
-    Pedido pedido = new Pedido();
-
-    Produto p = new Produto("Café", 10.0);
-    pedido.adicionarItem(new ItemPedido(p, 2));
-
-    assertEquals(20.0, pedido.calcularTotal());
-}
