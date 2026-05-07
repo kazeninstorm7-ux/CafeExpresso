ed8c978e0d60829d7d01f348bf3f3299753e9338
kazeninstorm7-ux <kazeninstorm7@gmail.com>
2026-05-07 14:59:28 -0300

    Create ItemPedido.java
    Implementa classe ItemPedido

---
+++ ItemPedido.java
@@ -0,0 +1,28 @@
+public class ItemPedido {
+
+    Produto produto;
+    int quantidade;
+
+    public ItemPedido(Produto p, int q) {
+
+        produto = p;
+
+        if (q <= 0) {
+            quantidade = 1;
+        } else {
+            quantidade = q;
+        }
+    }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
